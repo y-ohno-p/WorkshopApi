397a77e5c12e2f2cfcc85b641f481351c95b1e8a
大野 洋平 <y-ohno@pca.co.jp>
2022-02-21 18:06:42 +0900

    APIエンドポイントを変更

--- Startup.cs
+++ Startup.cs
@@ -1,16 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace WorkshopApi
 {
@@ -26,7 +19,6 @@ namespace WorkshopApi
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-
 			services.AddControllers();
 			services.AddSwaggerGen(c =>
 			{
@@ -41,7 +33,7 @@ namespace WorkshopApi
 			{
 				app.UseDeveloperExceptionPage();
 				app.UseSwagger();
-				app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WorkshopApi v1"));
+				app.UseSwaggerUI(c => c.SwaggerEndpoint("/api/swagger/v1/swagger.json", "WorkshopApi v1"));
 			}
 
 			app.UseHttpsRedirection();
