c3ce97dc0553773d91dbe0fbe9fe270d5f3b43da
大野 洋平 <y-ohno@pca.co.jp>
2022-02-22 19:05:53 +0900

    ログの記録

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -7,6 +7,7 @@ namespace WorkshopApi.Controllers
 {
 	[ApiController]
 	[Route("api/[controller]")]
+	//[Route("[controller]")] // デバッグ実行時は api を抜かないと動作しない
 	public class WeatherForecastController : ControllerBase
 	{
 		private static readonly string[] Summaries = new[]
@@ -17,6 +18,8 @@ namespace WorkshopApi.Controllers
 		[HttpGet]
 		public IEnumerable<WeatherForecast> Get()
 		{
+			Console.WriteLine($"Called {Request.Method} {Request.Host}{Request.Path} by {Request.Headers["X-Forwarded-For"]}");
+
 			var rng = new Random();
 			return Enumerable.Range(1, 5).Select(index => new WeatherForecast
 			{
